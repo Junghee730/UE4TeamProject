5fbdcf5acaff8bc58e1cb9df5cd596fe3aafc6ae
Junghee730 <75735055+Junghee730@users.noreply.github.com>
2022-12-11 04:48:42 +0900

    level design+asset
    asset

--- Source/UE4Project/PlayerGameInstanceSubsystem.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-//¹Ú¿¬Áö
-
-#include "PlayerGameInstanceSubsystem.h"
-
-UPlayerGameInstanceSubsystem::UPlayerGameInstanceSubsystem() : playerPower(100)
-{
-
-}
--- Source/UE4Project/UE4Project.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UE4Project.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4Project, "UE4Project" );
- 
